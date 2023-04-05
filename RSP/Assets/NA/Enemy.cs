de2a8b5cfd994ed2e749a1eee6e919c95a8952dd
Legnic <0415ksm@gmail.com>
2023-04-05 09:09:33 +0900

    잠시 주석처리

--- RSP/Assets/NA/Enemy.cs
+++ RSP/Assets/NA/Enemy.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Enemy : Character
-{
-    void TurnOver(int damage)
-    {
-        Health -= damage;
-    }
-}
+//public class Enemy : Character
+//{
+//    void TurnOver(int damage)
+//    {
+//        Health -= damage;
+//    }
+//}
