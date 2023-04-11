407e90c34358804aa6c62751b64331bcbac6d691
jsm <cokesoda2@naver.com>
2023-04-11 13:59:30 +0900

    Feat : 프로토타입 찐완성

--- RSP/Assets/JIN/Scripts/CustomCursor.cs
+++ RSP/Assets/JIN/Scripts/CustomCursor.cs
@@ -1,9 +1,33 @@
 using UnityEngine;
 
-public class CustomCursor : MonoBehaviour
+public class CustomCursor : Singleton<CustomCursor>
 {
+    private static CustomCursor _instance;
+
     public Texture2D defaultCursor;
     public Texture2D clickCursor;
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as CustomCursor;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Debug.Log("같은 타입의 Singleton이 이미 존재합니다. 새로 생성된 오브젝트를 삭제합니다.");
+            Destroy(gameObject);
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (_instance == this)
+        {
+            _instance = null;
+        }
+    }
 
     void Start()
     {
