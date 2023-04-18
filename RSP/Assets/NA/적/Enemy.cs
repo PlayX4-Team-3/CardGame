b668f5aca9f1be6b38c4e79e5532c20e832e36c7
MYNAMEISNINY <skguddnr1106@gmail.com>
2023-04-18 16:23:16 +0900

    Feat : Player, Enemy HpBar 구현중

--- RSP/Assets/NA/적/Enemy.cs
+++ RSP/Assets/NA/적/Enemy.cs
@@ -12,17 +12,31 @@ namespace AllCharacter
         private Text hpText;
         [SerializeField]
         private Text dfText;
-        [SerializeField]
-        private Slider hpSlider;
+
+        public Slider hpBar;
 
         private GameData data;
 
         private void Awake()
         {
+            //체력 초기화
             InitHp(40);
+            //방어력 초기화
             Defense_Figures = 0;
-            hpSlider.value = Hp;
+
+            //HP Sldier Value 초기화
+            hpBar.maxValue = Hp;
         }
+
+        private void Update()
+        {
+            hpBar.value = Hp;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Hp -= 1;
+            }
+        }
+
 
         public void DataInit(GameData data)
         {
