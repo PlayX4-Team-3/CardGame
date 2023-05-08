de081406eb3cc40f8a69ac6e268285b67b325774
jsm <cokesoda2@naver.com>
2023-05-08 22:56:00 +0900

    타이틀 버튼 수정

--- RSP/Assets/JIN/Scripts/ButtonAnimation.cs
+++ RSP/Assets/JIN/Scripts/ButtonAnimation.cs
@@ -8,54 +8,77 @@ public class ButtonAnimation : MonoBehaviour
 {
     public Button btn, ntb;
 
-    private bool isUp = false;
-    private bool isMouseExit = false;
+    private bool isUp1 = false;
+    private bool isMouseExit1 = false;
+
+    private bool isUp2 = false;
+    private bool isMouseExit2 = false;
 
     private void Update()
     {
-        if (isUp)
+        if (isUp1)
         {
             btn.GetComponent<Image>().fillAmount += 3f * Time.deltaTime;
             if (btn.GetComponent<Image>().fillAmount >= 1f)
             {
                 btn.GetComponent<Image>().fillAmount = 1f;
-                isUp = false;
-            }
-            ntb.GetComponent<Image>().fillAmount += 3f * Time.deltaTime;
-            if (ntb.GetComponent<Image>().fillAmount >= 1f)
-            {
-                ntb.GetComponent<Image>().fillAmount = 1f;
-                isUp = false;
+                isUp1 = false;
             }
         }
 
-        if (isMouseExit)
+        if (isMouseExit1)
         {
             btn.GetComponent<Image>().fillAmount -= 3f * Time.deltaTime;
             if (btn.GetComponent<Image>().fillAmount <= 0f)
             {
                 btn.GetComponent<Image>().fillAmount = 0f;
-                isMouseExit = false;
+                isMouseExit1 = false;
             }
+        }
+
+        if (isUp2)
+        {
+            ntb.GetComponent<Image>().fillAmount += 3f * Time.deltaTime;
+            if (ntb.GetComponent<Image>().fillAmount >= 1f)
+            {
+                ntb.GetComponent<Image>().fillAmount = 1f;
+                isUp2 = false;
+            }
+        }
+
+        if (isMouseExit2)
+        {
             ntb.GetComponent<Image>().fillAmount -= 3f * Time.deltaTime;
             if (ntb.GetComponent<Image>().fillAmount <= 0f)
             {
                 ntb.GetComponent<Image>().fillAmount = 0f;
-                isMouseExit = false;
+                isMouseExit2 = false;
             }
         }
 
     }
 
-    public void ASD()
+    public void Btn1Up()
     {
-        isUp = true;
-        isMouseExit = false;
+        isUp1 = true;
+        isMouseExit1 = false;
     }
 
-    public void QWE()
+    public void Btn1Exit()
     {
-        isMouseExit = true;
-        isUp = false;
+        isMouseExit1 = true;
+        isUp1 = false;
+    }
+
+    public void Btn2Up()
+    {
+        isUp2 = true;
+        isMouseExit2 = false;
+    }
+
+    public void Btn2Exit()
+    {
+        isMouseExit2 = true;
+        isUp2 = false;
     }
 }
